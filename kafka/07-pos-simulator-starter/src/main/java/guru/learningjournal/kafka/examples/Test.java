af40bcbc37f324c6266a5223d6cdae693bb0898f
Zybin <anton.zybin@teradata.com>
2021-02-03 18:27:30 +0300

    update

---
+++ kafka/07-pos-simulator-starter/src/main/java/guru/learningjournal/kafka/examples/Test.java
@@ -0,0 +1,10 @@
+package guru.learningjournal.kafka.examples;
+
+import guru.learningjournal.kafka.examples.datagenerator.InvoiceGenerator;
+
+public class Test {
+    public static void main (String[] args) {
+        InvoiceGenerator ig = new InvoiceGenerator();
+
+    }
+}
